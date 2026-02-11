0c521c0654ae2e4999803355a5f4130b83a93425
spl357 <github@spl.us>
2026-02-10 20:09:02 -0500

    Fixed issue #1 and added progress bars.

--- scripts/unflatten_datasets.pl
+++ scripts/unflatten_datasets.pl
@@ -18,16 +18,16 @@ sub make_numbered_dirs {
 	chomp(my @file_names = `ls $path`);
 	my $dir_count = scalar @file_names / $batch_size;
 	print "Found " . scalar @file_names . " files. With a batch size of $batch_size, $dir_count directories are needed. Creating them...\n";
-	for(my $i=0; $i < $dir_count - 1; $i++) {
+	for(my $i=0; $i < $dir_count; $i++) {
 		my $dir_name = sprintf("%0.4d", $i);
 		if ( ! -d "$dir_name") {
-			print "Creating directory $dir_name\n";
 			mkdir $path . "/" . $dir_name or die("mkdir operation failed: $!\n");
+			update_progress_bar($i, int($dir_count));
 		} else {
 			die "Error: directory $dir_name already exists and it shouldn't. The destination directory need to be cleaned up by hand.\n";
 		}
 	}
-	print "Done creating $dir_count sub-directories. Moving files into new sub-directories...\n";
+	print "\n\nDone creating " . int($dir_count) . " sub-directories. Migrating files...\n";
 	return;
 }
 
@@ -42,13 +42,13 @@ sub move_files_to_numbered_dirs {
 
 	for (my $i = 0; $i <= $num_batches; $i++) {
 		my $dir_name = sprintf("%0.4d",$i);				
-		for (my $j = 0; $j <= $batch_size; $j++) {
+		for (my $j = 0; $j < $batch_size; $j++) {
 			my $file_name = shift(@file_names);
 			if(defined $file_name) {
 				chomp $file_name;
-				print "Moving $file_name to $base_path$dir_name\n";
 				move("$file_name", "$base_path$dir_name/")
 					or die "move operation failed: $!";
+				update_progress_bar($i, $num_batches);	
 			} else {
 				next; 
 			}
@@ -56,6 +56,14 @@ sub move_files_to_numbered_dirs {
 	}
 	print "All operations completed.\n";
 	return;
+}
+
+sub update_progress_bar {
+    my ($current, $total) = @_;
+    my $percent = int($current / $total * 100);
+    my $bar_length = 50;
+    my $filled = int($percent / (100 / $bar_length));
+    printf "\r[%*s] %3d%%", $bar_length, '.' x $filled . ' ' x ($bar_length - $filled), $percent;
 }
 
 1;
